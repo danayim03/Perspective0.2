b120022eeb6999d70262fa379f54c76ea350231f
Dana <danayim@bu.edu>
2025-02-28 00:05:10 -0500

    get rid of rematch button

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -387,16 +387,16 @@ export const ChatRoom = ({ userRole, onGoBack, onRematch, ws }: ChatRoomProps) =
                 </DropdownMenuContent>
               </DropdownMenu>
               
-              <Button
+              {/* <Button
                 onClick={handleRematchClick}
                 variant="ghost"
                 size="sm"
                 disabled={chatEnded}
                 className="text-perspective-600 hover:text-perspective-700 hover:bg-perspective-100 text-xs sm:text-sm py-1 px-2 sm:py-2 sm:px-3"
               >
-                {/* <RefreshCw className="w-3 h-3 sm:w-4 sm:h-4 mr-1" /> */}
+                <RefreshCw className="w-3 h-3 sm:w-4 sm:h-4 mr-1" />
                 <span className="hidden xs:inline">Rematch</span>
-              </Button>
+              </Button> */}
             </div>
           </div>
 
